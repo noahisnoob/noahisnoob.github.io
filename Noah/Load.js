83ac916c3b4356f76f38e081edfb1a8ec1c6fd09
noahisnoob <34514546+noahisnoob@users.noreply.github.com>
2019-07-13 13:38:29 +0100

    Delete Load.js

--- Noah/Load.js
+++
@@ -1,8 +0,0 @@
-window.loaded = true;
-$("#imagebig").fadeOut("slow", function() {
-
-});
-setTimeout(function() {
-    $("#imagebig").remove();
-    $("#preshow").css("display", "block");
-}, 2500);
